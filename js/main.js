dae10e63a8b31ad75c955dd7c512c44a37af011f
Rene Andrade <reneaugusto_@hotmail.com>
2020-08-02 17:18:35 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 30
+	}, 1500);
+})
+      
